3fba33cc6c97b9b91108c49493c4820b7fe31565
Mert Arguc <arguc.mert@gmail.com>
2022-01-21 10:44:46 +0300

    article router eklendi

--- router/index.ts
+++ router/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { getUsers,addUser,updateUser } from '../controllers/userController'
+import { getArticles,addArticle,updateArticle } from '../controllers/articleContoller'
 const router = Router();
 
 router.route('/').get(getUsers).post(addUser).put(updateUser)
+router.route('/article').get(getArticles).post(addArticle).put(updateArticle)
 
 export default router;
